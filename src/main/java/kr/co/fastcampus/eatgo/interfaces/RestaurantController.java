82a78c21587a5f5443c39795bc2eee0902351d5a
JerryKwon <kwonyoung234@gmail.com>
2020-02-05 22:51:35 +0900

    스프링부트 17강 프론트엔드

--- src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin
 @RestController
 public class RestaurantController{
 
